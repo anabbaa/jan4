4884b07afb4d2727deb2a2f2821e88389a5c950a
ahmed nabbaa <ahmed.nabaa@outlook.com>
2021-01-05 20:35:36 +0100

    second

---
+++ index.js
@@ -0,0 +1,36 @@
+
+let str = "i can walk in the park all the day";
+let stro = str.substring(18,22);
+console.log(stro);
+let str1 = "hello world";
+strl2 = str1.toUpperCase();
+console.log(strl2);
+let strl3 = "earthings";
+strl4 = strl3.toLowerCase();
+console.log(strl4);
+let string = "JavaScript";
+string1 = string.substring(3,6);
+console.log(string1);
+let text = "nice shoes";
+let text1 = text.includes("l")
+console.log(text1);
+console.log("J" + string + "J" );
+let lastthree = string.substring(string.length -3);
+let newjava = lastthree + string + lastthree;
+console.log(newjava);
+let firstchr = string[0];
+let lastchr = string[string.length -1];
+let mod1 = string.substring(1, string.length -1);
+let finalm = lastchr + mod1 + firstchr;
+console.log(finalm);
+let strcex = "isfun";
+console.log(strcex.toUpperCase());
+console.log(strcex.includes("java"));
+let uname = "ahmed ";
+let add = "Hamburg";
+let job = "teacher";
+let aboutme = `My name is ${uname}, i live in ${add}, iam a ${job}.`;
+console.log(aboutme);
+let strcec12 = "the quick brown fox";
+console.log(strcec12[0].toUpperCase() + strcec12.substring(1));
+
